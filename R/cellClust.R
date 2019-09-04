e02bec551ab77de8e47afadd75b6c4547c442142
Florian <floriangfl@aol.com>
2019-09-04 12:22:21 +0800

    Changed the clustering

--- R/cellClust.R
+++ R/cellClust.R
@@ -14,38 +14,24 @@
 #' 
 cellClust <- function(obj_in,method="hclust",deepSplit_wgcna=1,min_group_Size_wgcna=5)
 {
-  #### 1: reading input ####  
-  fpkm_temp = obj_in$fpkm_for_clust;
+  fpkm_temp = obj_in$fpkm_for_clust
   
-  if (!require(flashClust)) install.packages("flashClust",repos = "http://cran.us.r-project.org") 
-  require(flashClust)
+  if (!require(fastcluster)) install.packages("fastcluster",repos = "http://cran.us.r-project.org") 
+  require(fastcluster)
   if (!require(WGCNA)){
-    source("http://bioconductor.org/biocLite.R");
-    biocLite(c("impute", "GO.db", "preprocessCore")); 
-    install.packages("WGCNA");
+    source("http://bioconductor.org/biocLite.R")
+    biocLite(c("impute", "GO.db", "preprocessCore"))
+    install.packages("WGCNA")
   }
   require(WGCNA)  
-  
-  #### 2: choosing method ####
-    if (method == "hclust"){
-        d = as.dist(1-cor(fpkm_temp,method="pearson"));
-        cellTree = flashClust(d,method = "average");     
-        dynamicGroups = cutreeDynamic(dendro = cellTree,distM = as.matrix(d),deepSplit = deepSplit_wgcna,
-                                      pamStage = FALSE,minClusterSize= min_group_Size_wgcna);
-        dynamicColors = labels2colors(dynamicGroups);
-        group_labels = matrix(dynamicGroups,nrow = length(names(fpkm_temp)),ncol = 1,list(names(fpkm_temp),c("groupLabel")),byrow=FALSE)
-        group_labels = as.data.frame(group_labels)
-        group_labels_color = cbind(group_labels,dynamicColors);
-    }
-  #### 3: writing output ####    
-  obj_out = append(obj_in,
-              list("d" = d,
-              "cellTree" = cellTree,
-              "dynamicColors" = dynamicColors,
-              "group_labels_color" = group_labels_color
-              )
-  )
-  
-  return(obj_out)
-  
+  if (require(HiClimR)){
+  d = as.dist(1-fastCor(fpkm_temp,upperTri=TRUE, nSplit=5, optBLAS = T))
+  }else{
+  d = as.dist(1-cor(fpkm_temp,method="pearson"))
+  }
+  cellTree = fastcluster::hClust(d,method = "average")     
+  dynamicGroups = cutreeDynamic(dendro = cellTree,distM = as.matrix(d),deepSplit = deepSplit_wgcna,pamStage = FALSE,minClusterSize= min_group_Size_wgcna)
+  dynamicColors = labels2colors(dynamicGroups)
+    
+  return(list("d" = d,"cellTree" = cellTree,"dynamicColors" = dynamicColors,"group_labels_color" = group_labels_color))
 }
