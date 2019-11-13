3743577fe7fda7f1d8884c02ba84d064e44c5514
Florian <floriangfl@aol.com>
2019-11-13 16:12:40 +0800

    Added show function

--- R/RCA-class.R
+++ R/RCA-class.R
@@ -2,4 +2,11 @@
 #'
 #' @export
 #'
-RCAConstruct <- setRefClass(Class = "RCA", fields = list(raw.data = "Matrix", data = "Matrix", projection.data = "Matrix", clustering.out = "list"))
+RCAConstruct <- setRefClass(Class = "RCA", 
+			    fields = list(raw.data = "Matrix", 
+					  data = "Matrix", 
+					  projection.data = "Matrix", 
+					  clustering.out = "list"))
+
+RCAConstruct$methods(show=function(){dataSize=dim(raw.data);
+		     print(paste0("RCA reference class object (version 2.0) holding ",dataSize[2]," cells and ",dataSize[1],"features."))})
