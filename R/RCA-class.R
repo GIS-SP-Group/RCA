6eeeb131414567eed638d96b2691434ee9fbff0f
Florian <floriangfl@aol.com>
2019-11-14 15:57:08 +0800

    Fixed a bug in show

--- R/RCA-class.R
+++ R/RCA-class.R
@@ -12,17 +12,15 @@ RCAConstruct$methods(show=function(){
 		     print("RCA reference class object")
 		     if (!(is.null(raw.data))){
 			     dataSize=dim(raw.data);
-			     print(paste0("Raw data: ",dataSize[2]," cells and ",dataSize[1]," features."))}
+			     print(paste0("Raw data: ",dataSize[2]," cells and ",dataSize[1]," features."))
 			    }
                      if (!(is.null(data))){
 		     dataSize=dim(data);
-			     print(paste0("Filtered data: ",dataSize[2]," cells and ",dataSize[1]," features."))}
-	
+			     print(paste0("Filtered data: ",dataSize[2]," cells and ",dataSize[1]," features."))
+		     }
 		     if (!(is.null(projection.data))){
 		     dataSize=dim(projection.data);
 			     print(paste0("Projection data: ",dataSize[2]," cells to ",dataSize[1]," cell-types."))}
-		     
-		     
-		     )
+		     })
 
 		      
