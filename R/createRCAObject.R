4e35ab83961b86dc1222cc6a0fb284809f3c3ad9
Florian <floriangfl@aol.com>
2019-12-10 16:30:16 +0800

    Added a funciton to add normalized data

--- R/createRCAObject.R
+++ R/createRCAObject.R
@@ -4,11 +4,15 @@
 #' @return RCA object.
 #' @export
 #'
-createRCAObject <- function(rawData) {
+createRCAObject <- function(rawData,dataIsNormalized=FALSE) {
 
     # Create RCA object using RCAConstruct and the raw data provided
+    if (dataIsNormalized){
+    rca.obj <- RCAConstruct$new(raw.data = rawData, data=rawData)
+    }else
+    {
     rca.obj <- RCAConstruct$new(raw.data = rawData)
-
+    }
     # Return RCA object
     return(rca.obj)
 }
--- R/plotRCAHeatmap.R
+++ R/plotRCAHeatmap.R
@@ -90,8 +90,7 @@ plotRCAHeatmap <- function(rca.obj, var.thrs = 0.1, width = 20, height = 20, fol
         names(clusterColorDf) <- names(clusterColorList)
 
         # Create cell property list - list of NODG, nUMI and percent.mito
-	print(head(rca.obj$raw.data))
-        nUMI <- colSums(rca.obj$raw.data)
+        nUMI <- colSums(as.matrix(rca.obj$raw.data))
         nodg <- colSums(rca.obj$data > 0)
 
         mito.genes = grep(pattern = "^MT-", x = rownames(rca.obj$data), value = T)
