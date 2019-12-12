e02b36e92ac60058162a123a59a7387df6a89e65
Florian <floriangfl@aol.com>
2019-12-12 17:19:22 +0800

    Fixed a bug in color assignment

--- R/plotRCAUMAP.R
+++ R/plotRCAUMAP.R
@@ -84,8 +84,12 @@ plotRCAUMAP <- function(rca.obj, cellPropertyList = NULL, folderpath = ".", file
             # Create the plot
             umapClusterColorsPlot <- ggplot(data = umap.df, mapping = aes(x = UMAP1, y = UMAP2, colour = unlist(rRank))) + geom_point(size = .5) + scale_color_identity() +  theme_bw() +ggtitle("a)")
 
-            umapClusterColorsPlot2 <- ggplot(data = umap.df, mapping = aes(x = UMAP1, y = UMAP2, colour = unlist(rBaseColors))) + geom_point(size = .5) + scale_color_manual(values=sort(unique(unlist(rBaseColors)))) +  theme_bw() + ggtitle("b)")+
-		    theme(legend.position="left")
+
+	    names(rBaseColors)<-NULL
+	    colorOrder<-order(unique(unlist(rBaseColors)))
+	    colorVec<-unique(unlist(rBaseColors))[colorOrder]
+	    names(colorVec)<-unique(names(unlist(rBaseColors)))[colorOrder]
+            umapClusterColorsPlot2 <- ggplot(data = umap.df, mapping = aes(x = UMAP1, y = UMAP2, colour = unlist(rBaseColors))) + geom_point(size = .5) + scale_color_identity(labels=names(colorVec)) +  theme_bw() + ggtitle("b)")+   theme(legend.position="right")+labs(color="Cell type")
 
             # Save plot
 	    pdf(paste0(folderpath, "/", "RelativeRank_", filename),width=14,height=7)
