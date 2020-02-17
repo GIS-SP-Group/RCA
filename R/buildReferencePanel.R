985d044ba288e20999b4dfc0651b2e3f325492a4
Bobby Ranjan <bbbranjan@gmail.com>
2020-02-17 10:14:08 +0800

    Added CPM normalization and handling of ENS IDs for human and mouse data.

--- R/buildReferencePanel.R
+++ R/buildReferencePanel.R
@@ -4,19 +4,21 @@
 #' @param celltype.vec Vector of cell type names of each bulk RNA sequencing sample.
 #' @param fc.thrs fold change threshold
 #' @param fdr.thrs FDR threshold
+#' @param gene.nomenclature Type of gene nomenclature. "ENS" for Ensembl IDs, "SYMBOL" for gene symbols. Default is "SYMBOL".
+#' @param species "HUMAN" for human data, "MOUSE" for mouse data. Default is "HUMAN".
 #' @param folder.path path to store reference panel
 #' @param filename file name of reference panel (should end with .rds)
 #' 
 #' @export
 #' 
 
-buildReferencePanel <- function(bulk.rna.data, celltype.vec = colnames(bulk.rna.data), fc.thrs = 7.5, fdr.thrs = 0.0001, folder.path = ".", filename = "my_reference_panel.rds") {
+buildReferencePanel <- function(bulk.rna.data, celltype.vec = colnames(bulk.rna.data), fc.thrs = 7.5, fdr.thrs = 1e-02, gene.nomenclature = "SYMBOL", species = "HUMAN", folder.path = ".", filename = "my_reference_panel.rds") {
     
     if(ncol(bulk.rna.data) != length(celltype.vec)) {
         stop("Data doesn't match cell type vector.")
     }
     
-    ## Name bulk RNA replicates using cell type vector
+    # Name bulk RNA replicates using cell type vector
     celltype.vec <- gsub(pattern = "_", replacement = "\\.", x = celltype.vec)
     
     unique.ct <- unique(celltype.vec)
@@ -28,25 +30,26 @@ buildReferencePanel <- function(bulk.rna.data, celltype.vec = colnames(bulk.rna.
         colnames(bulk.rna.data)[i] <- paste0(celltype.vec[i], "_", ct.list[[celltype.vec[i]]])
     }
     
-    
-    ## Identify genes with at least 0.5 cpm in at least 2 samples
-    thresh <- bulk.rna.data > 0.5
-    keep <- rowSums(thresh) >= 2
-    
-    # Subset the rows of countdata to keep the more highly expressed genes
-    filt.bulk.rna.data <- bulk.rna.data[keep,]
-    
     library(edgeR)
     
-    ## Convert to an edgeR object
-    dgeObj <- DGEList(filt.bulk.rna.data, group = celltype.vec)
+    # Convert to an edgeR object
+    dgeObj <- DGEList(bulk.rna.data, group = celltype.vec)
+    
+    
+    ## Identify genes with at least 1 cpm in at least 2 samples
+    cpm.bulk.rna.data <- cpm(dgeObj)
+    thresh <- cpm.bulk.rna.data > 1
+    keep <- rowSums(thresh) >= 2
+    
+    # Filter genes based on expression
+    dgeObj <- dgeObj[keep, ]
     
     # Estimate common and tag-wise dispersion for the pair of clusters
     dgeObj <- estimateCommonDisp(dgeObj)
     dgeObj <- estimateTagwiseDisp(dgeObj)
     
     # Calculate norm factors for data
-    dgeObj <- calcNormFactors(dgeObj, method = "none")
+    dgeObj <- calcNormFactors(dgeObj, method = "TMM")
     
     etList <- list()
     for(i in 1:(length(unique.ct)-1)) {
@@ -70,7 +73,7 @@ buildReferencePanel <- function(bulk.rna.data, celltype.vec = colnames(bulk.rna.
     de.union <- unique(unlist(de.list))
     
     # Define reference panel
-    ref.panel <- filt.bulk.rna.data[de.union, ]
+    ref.panel <- cpm.bulk.rna.data[de.union, ]
     
     # Average out replicates
     average.ref.panel <- matrix(0, nrow(ref.panel), ncol = length(unique.ct))
@@ -84,7 +87,33 @@ buildReferencePanel <- function(bulk.rna.data, celltype.vec = colnames(bulk.rna.
         }
         
     }
-
+    
+    # For Ensembl gene IDs
+    if(gene.nomenclature == "ENS") {
+        ensembl_genes <- grep(pattern = "^ENS", x = rownames(average.ref.panel), value = T)
+        
+        # if Ensembl gene version numbers exist, remove them
+        ensembl_genes <- gsub(pattern = "\\..*$", replacement = "", x = ensembl_genes)
+        
+        if(length(ensembl_genes) != length(unique(ensembl_genes))) {
+            stop("Non-unique gene names in input data.")
+        }
+        
+        rownames(average.ref.panel) <- ensembl_genes
+        
+        if(species == "HUMAN") {
+            gene.id.df <- ensembldb::select(EnsDb.Hsapiens.v86::EnsDb.Hsapiens.v86, keys= ensembl_genes, keytype = "GENEID", columns = c("SYMBOL","GENEID"))
+        } else if(species == "MOUSE") {
+            gene.id.df <- ensembldb::select(EnsDb.Mmusculus.v79::EnsDb.Mmusculus.v79, keys= ensembl_genes, keytype = "GENEID", columns = c("SYMBOL","GENEID"))
+        }
+        
+        average.ref.panel <- average.ref.panel[gene.id.df$GENEID, ]
+        
+        # Removing duplicated gene symbols
+        average.ref.panel <- average.ref.panel[-which(duplicated(gene.id.df$SYMBOL)), ]
+        rownames(average.ref.panel) <- gene.id.df$SYMBOL[-which(duplicated(gene.id.df$SYMBOL))]
+        
+    }
     
     # Save reference panel
     saveRDS(object = average.ref.panel, file = paste0(folder.path, "/", filename))
