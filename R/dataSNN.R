83b6fbeea43d99723a85a7491f0c772b286264a6
Florian411 <fschmidt@mmci.uni-saarland.de>
2020-05-08 10:47:19 +0800

    Added correlation based distance to graph based clustering

--- R/dataSNN.R
+++ R/dataSNN.R
@@ -4,19 +4,51 @@
 #' @param k Number of cells to consider in the neirest neighbour graph. Default is 10.
 #' @param eps Number of cells that have to be similar to connect 2 cells in a network. Default is 8
 #' @param minPts Number of cells with at least eps neighbours to be considered a core point. Default is 5.
+#' @param dist.fun Either PCA or All. For PCA, a PCA reduction of the projection will be performed before the correlation matrix is computed.
 #' @return RCA object.
 #' @export
 #'
-dataSNN <- function(rca.obj,k=10,eps=8,minPts=5) {
-
-    # Extract projection data
+dataSNN <- function(rca.obj,k=10,eps=8,minPts=5,dist.fun="All") {
+    if (dist.fun=="PCA"){
+    # Extract projection data 
+    print("Computing PCA on projection")
     projection.data <- as.matrix(rca.obj$projection.data)
     pcaD = stats::prcomp(projection.data)
     components=c(1:(max(which(summary(pcaD)$importance[3,]<0.99))+1))
+
+    if (require(HiClimR)) {
+        d = as.dist(1 - HiClimR::fastCor(
+            pcaD$rotation[,components],,
+            upperTri = TRUE,
+            nSplit = 5,
+            optBLAS = T
+        ))
+    } else {
+        # else, use cor
+        d = as.dist(1 - cor(pcaD$rotation[,components], method = "pearson"))
+    }
+ 
+    # Obtain cell tree using a reduced projection matrix
+    clusteringResult<-dbscan::sNNclust(d,k,eps,minPts,borderPoints = T)
+    } else {
+    print("Compute correlation distance matrix from projection")
+    # If HiClimR is available, use fastCor to compute distance matrix
+    if (require(HiClimR)) {
+        d = as.dist(1 - HiClimR::fastCor(
+            projection.data,
+            upperTri = TRUE,
+            nSplit = 5,
+            optBLAS = T
+        ))
+    } else {
+        # else, use cor
+        d = as.dist(1 - cor(projection.data, method = "pearson"))
+    }
     
     # Obtain cell tree using a reduced projection matrix
-    clusteringResult<-dbscan::sNNclust(pcaD$rotation[,components],k,eps,minPts,borderPoints = T)
-
+    clusteringResult<-dbscan::sNNclust(d,k,eps,minPts,borderPoints = T)
+}
+    
     # Convert labels to colours for each tree cut
     clusterColors<-randomcoloR::distinctColorPalette(length(unique(clusteringResult$cluster)))
     clusterColors<-sapply(clusterColors,plotrix::color.id)
