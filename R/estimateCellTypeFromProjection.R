05c7a2057c3a93698470e5ec482e191d36961123
Florian411 <fschmidt@mmci.uni-saarland.de>
2020-05-19 15:20:47 +0800

    Bugfix in field assignments

--- R/estimateCellTypeFromProjection.R
+++ R/estimateCellTypeFromProjection.R
@@ -74,7 +74,7 @@ estimateCellTypeFromProjection <- function(rca.obj, confidence = NULL, ctRank = 
             }
             rca.obj$cScore <- confidenceScore
         } else{
-            rca.obj$cScore <- list()
+            rca.obj$cScore <- NULL
         }
         if (ctRank) {
             myColors <-
@@ -91,8 +91,8 @@ estimateCellTypeFromProjection <- function(rca.obj, confidence = NULL, ctRank = 
             }
             rca.obj$rRank <- relativeColorRank
         } else{
-            rca.obj$rRank <- list()
-            rca.obj$baseColors <- list()
+            rca.obj$rRank <- NULL
+            rca.obj$baseColors <- NULL
         }
         
         
