97fc43752dbc28956e23be8d724a164c8410b3af
Florian411 <fschmidt@mmci.uni-saarland.de>
2020-07-09 18:54:05 +0800

    Changes to CMake files

--- R/computePairDE.R
+++ R/computePairDE.R
@@ -111,7 +111,6 @@ dataDE <- function(rca.obj,
             if (!(is.null(marker.genes))) {
                 if (colnames(marker.genes)[1] != 'myAUC') {
                     marker.genes = marker.genes[marker.genes$p_val_adj < 0.05, ]
-	    	    tmp=rbind(tmp, marker.genes)
                 }
                 if(nrow(marker.genes) > 0) {
                     marker.genes$group1 = clusteri
