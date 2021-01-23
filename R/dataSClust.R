2765ca26e6ecbfc61dc8fcc82db143c934557244
Florian411 <fschmidt@mmci.uni-saarland.de>
2021-01-23 16:00:06 +0800

    Pushed new clustering

--- R/dataSClust.R
+++ R/dataSClust.R
@@ -1,50 +1,23 @@
-#Modified from https://www.r-bloggers.com/2013/02/bigcor-large-correlation-matrices-in-r/
-bigcor <- function(x, nblocks = 10, verbose = TRUE, ...)
-{
-  require(ff)
-  NCOL <- ncol(x)
-    ## test if ncol(x) %% nblocks gives remainder 0
-    if (NCOL %% nblocks != 0) stop("Choose different 'nblocks' so that ncol(x) %% nblocks = 0!")
-    ## preallocate square matrix of dimension
-    ## ncol(x) in 'ff' single format
-    corMAT <- ff(vmode = "single", dim = c(NCOL, NCOL))
-      ## split column numbers into 'nblocks' groups
-      SPLIT <- split(1:NCOL, rep(1:nblocks, each = NCOL/nblocks))
-      ## create all unique combinations of blocks
-      COMBS <- expand.grid(1:length(SPLIT), 1:length(SPLIT))
-        COMBS <- t(apply(COMBS, 1, sort))
-        COMBS <- unique(COMBS)
-	  ## iterate through each block combination, calculate correlation matrix
-	  ## between blocks and store them in the preallocated matrix on both
-	  ## symmetric sides of the diagonal
-	  for (i in 1:nrow(COMBS)) {
-		COMB <- COMBS[i, ]
-		G1 <- SPLIT[[COMB[1]]]
-	        G2 <- SPLIT[[COMB[2]]]
-		flush.console()
-		COR <- cor(x[, G1], x[, G2], ...)
-		corMAT[G1, G2] <- COR
-		corMAT[G2, G1] <- t(COR)
-		COR <- NULL
-		}
-	  gc()
-	  return(corMAT)
-}
-
-
 #' Generate elbow plot for a PCA of the projection matrix.
 #'
 #' @param rca.obj RCA object.
 #' @param nPCs Number of PCs to be used if distance should be computed in PC embedding of the projection (default 0, computation of distance in projection space)
+#' @param approx Flag indicating wheter an approximation of the SVD should be used
 #' @return NULL.
 #' @export
 #'
-elbowPlot <- function(rca.obj,nPCs=50,filename="Projection_Elbow.png") {
-	tmp<-irlba::prcomp_irlba(as.matrix(rca.obj$projection.data),n=nPCs,center=F,scale.=F)
+elbowPlot <- function(rca.obj,nPCs=50,filename="Projection_Elbow.png",approx=F) {
+  if (approx){
+    tmp<-irlba(rca.obj$projection.data,nv=nPCs)
+    tmp$sdev <- tmp$d/sqrt(max(1, ncol(rca.obj$projection.data) - 1))
+  }
+  else{
+	  tmp<-irlba::prcomp_irlba(rca.obj$projection.data,n=nPCs,center=F,scale.=F)
+  }
 	png(filename)
 	plot(tmp$sdev,ylab="Standard deviation",xlab="Component")
 	dev.off()
-	return
+	return()
 }
 	
 
@@ -52,42 +25,58 @@ elbowPlot <- function(rca.obj,nPCs=50,filename="Projection_Elbow.png") {
 #'
 #' @param rca.obj RCA object.
 #' @param res Resolution parameter (between 0.0 and 1.0) to be used for clustering. Default: 0.5. Check the Seurat documentation for details.
-#' @param corMeth Correlation method used to compute the distance matrix of the projection (pearson (default), spearman, kendal).
-#' @param bigCor Number of blocks to be used in the divide and conquer bigcor function to compute the correlation distance (advisable for > 100.000 cells if PC embedding is not used, default 0)
-#' @param nPCs Number of PCs to be used if distance should be computed in PC embedding of the projection (default 0, computation of distance in projection space)
+#' @param corMeth Correlation method used to compute the distance matrix of the projection (none(default), pearson , spearman, kendal).
+#' @param nPCs Number of PCs to be used if distance should be computed in PC embedding of the projection (default 10)
+#' @param approx Using approximation for PCA computation (default T)
 #' @return RCA object.
 #' @export
 #'
-dataSClust <- function(rca.obj,res=0.5,corMeth="pearson",bigCor=0,nPCs=0) {
-	projection.data <- as.matrix(rca.obj$projection.data)
-	tempS<-Seurat::CreateSeuratObject(as.matrix(rca.obj$raw.data))
-	if (nPCs==0){
-		if (bigCor!=0){
-			bigM<-as.matrix(bigcor(projection.data,nblocks=bigCor)[,])
-			colnames(bigM)<-colnames(projection.data)
-			row.names(bigM)<-colnames(projection.data)
-			projection<-as.dist(1-bigM)
+dataSClust <- function(rca.obj,res=0.5, corMeth="none", bigCor=0, nPCs=10, approx=T) {
+ tempS<-Seurat::CreateSeuratObject(rca.obj$raw.data)
+ if(!(approx)){
+  if ((nPCs==0) & (corMeth !="none")){
+	  if (require(HiClimR) & (corMeth=="pearson")){
+		  projection<-as.dist(1-HiClimR::fastCor(projection.data))
+		  }else{
+			projection<-as.dist(1-cor(projection.data,method=corMeth))
+		  }
+  } else{
+    if (corMeth != "none"){
+		  pca_result<-cor(t(irlba::prcomp_irlba(projection.data,n=nPCs,center=F,scale.=F)$rotation),method=corMeth)
+		  colnames(pca_result)<-colnames(projection.data)
+		  row.names(pca_result)<-colnames(projection.data)
+		  projection<-as.dist(1-pca_result)
+		} else {
+		  if ((nPCs !=10)&(corMeth=="none")){
+        pca_result<-irlba::prcomp_irlba(projection.data,n=nPCs,center=F,scale.=F)$rotation
+        row.names(pca_result)<-colnames(projection.data)
+        projection<-pca_result}
+      }
+  }
+   tempS@reductions[["pca"]]<-new(Class = "DimReduc", cell.embeddings = matrix(0,0,0), assay.used = "RNA")
+   tempS@reductions$pca@cell.embeddings<-projection
+ }else{
+   #From Seurat RunPCA
+   npcs <- min(nPCs, nrow(rca.obj$projection.data) - 1)
+   pca.results <- irlba(A =t(rca.obj$projection.data), nv = npcs)
+   feature.loadings <- pca.results$v
+   sdev <- pca.results$d/sqrt(max(1, ncol(rca.obj$projection.data) - 1))
+   projection <- pca.results$u %*% diag(pca.results$d)
+   
+   rownames(x = feature.loadings) <- rownames(x = rca.obj$projection.data)
+   colnames(x = feature.loadings) <- paste0("PC_", 1:npcs)
+   rownames(x = projection) <- colnames(x = rca.obj$projection.data)
+   colnames(x = projection) <- colnames(x = feature.loadings)
+   total.variance <- sum(apply(X=rca.obj$projection.data,MARGIN=2,FUN=var))
+   tempS@reductions[["pca"]] <- Seurat::CreateDimReducObject(
+     embeddings = projection,
+     loadings = feature.loadings,
+     assay = "RNA",
+     stdev = sdev,
+     key = "PC_",
+     misc = list(total.variance = total.variance))
+	}
 
-		}else{
-			if (require(HiClimR) & (corMeth=="pearson")){
-				projection<-as.dist(1-HiClimR::fastCor(projection.data))
-			}else{
-				projection<-as.dist(1-cor(projection.data,method=corMeth))
-				}
-			}
-	}
-	else{
-		pca_result<-cor(t(irlba::prcomp_irlba(projection.data,n=nPCs,center=F,scale.=F)$rotation),method=corMeth)
-		colnames(pca_result)<-colnames(projection.data)
-		row.names(pca_result)<-colnames(projection.data)
-		projection<-as.dist(1-pca_result)
-	}
-	cat(dim(as.matrix(projection)))
-	cat(head(colnames(as.matrix(projection))))
-	cat(head(rownames(as.matrix(projection))))
-	str(projection)
-	tempS@reductions[["pca"]]<-new(Class = "DimReduc", cell.embeddings = matrix(0,0,0), assay.used = "RNA")
-	tempS@reductions$pca@cell.embeddings<-as.matrix(projection)
 	tempS<-Seurat::FindNeighbors(object = tempS)
 	tempS<-Seurat::FindClusters(tempS,resolution = res)
 
