b8300a966b5bd94ab5a2c9b3399ed9a43028934d
Florian411 <fschmidt@mmci.uni-saarland.de>
2021-01-25 14:48:38 +0800

    Added further UMAP speed up options

--- R/computeUMAP.R
+++ R/computeUMAP.R
@@ -3,9 +3,11 @@
 #' @param rca.obj RCA object
 #' @param nDIMS number of UMAP dimensions
 #' @param nThreads Number of threads to use for UMAP
+#' @param nThreadsSGD Number of threads used for stochastic gradient descent (WARNING Results are not reproducible if this is > 1)
+#' @param approxPow approximate the power function in stochastic gradient descent (Default FALSE)
 #' @export
 #'
-computeUMAP <- function(rca.obj, nDIMS=2, nThreads=1) {
+computeUMAP <- function(rca.obj, nDIMS=2, nThreads=1, nThreadsSGD=1, approxPow=FALSE) {
 
     ### Extract projection data from RCA object
     projection = as.matrix(rca.obj$projection.data)
@@ -16,7 +18,7 @@ computeUMAP <- function(rca.obj, nDIMS=2, nThreads=1) {
 	print("Error: nDIMS must be set to 2 or 3")
     	return(rca.obj)}
     else{
-        umap.projection <- umap::umap(t(projection),n_components=nDIMS, random_state=1,transform_state=1, n_threads = nThreads)
+        umap.projection <- umap::umap(t(projection),n_components=nDIMS, random_state=1,transform_state=1, n_threads = nThreads, n_sgd_threads = nThreadsSGD, approx_pow = approxPow)
     }
 
     # Store UMAP layout in data frame for plotting
