a025c68aae3a50ffba565f063368519946d5da3c
Florian Schmidt <floriangfl@aol.com>
2021-05-07 13:48:32 +0800

    Fixed data filer tests

--- tests/testthat/testDataFilter.R
+++ tests/testthat/testDataFilter.R
@@ -3,16 +3,16 @@ library(RCAv2)
 load("../testdata/pbmc_small_rca.rda")
 
 test_that("dataFilter returns RCA object even if no cells are filtered out", {
-    expect_s4_class(dataFilter(rca.obj = pbmc_small_rca, nGene.thresholds = c(0,Inf), nUMI.thresholds = c(0,Inf), percent.mito.thresholds = c(0,Inf)), class = "RCA")
+    expect_s4_class(dataFilter(rca.obj = pbmc_small_rca, nGene.thresholds = c(0,Inf), nUMI.thresholds = c(0,Inf), percent.mito.thresholds = c(0,Inf), plot = F), class = "RCA")
     expect_equal(ncol(dataFilter(rca.obj = pbmc_small_rca, nGene.thresholds = c(0,Inf), nUMI.thresholds = c(0,Inf), percent.mito.thresholds = c(0,Inf))$raw.data), 80)
 })
 
 test_that("dataFilter returns RCA object at default settings", {
-    expect_s4_class(dataFilter(rca.obj = pbmc_small_rca), class = "RCA")
+    expect_s4_class(dataFilter(rca.obj = pbmc_small_rca, plot = FALSE), class = "RCA")
 })
 
 
 test_that("dataFilter returns RCA object even if all cells are filtered out", {
-    expect_s4_class(dataFilter(rca.obj = pbmc_small_rca, nGene.thresholds = c(0,0)), class = "RCA")
-    expect_equal(ncol(dataFilter(rca.obj = pbmc_small_rca, nGene.thresholds = c(0,Inf), nUMI.thresholds = c(0,Inf), percent.mito.thresholds = c(0,Inf))$raw.data), 0)
+    expect_s4_class(dataFilter(rca.obj = pbmc_small_rca, nGene.thresholds = c(0,0), plot = FALSE), class = "RCA")
+    expect_equal(ncol(dataFilter(rca.obj = pbmc_small_rca, nGene.thresholds = c(0,0), plot = FALSE)$raw.data), 0)
 })
